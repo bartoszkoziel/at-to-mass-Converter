7211b14e7375e211b700930153c4c91f5d36ba5c
bartoszkoziel <kozielbartosz1@gmail.com>
2023-07-02 20:30:38 +0200

    added second frame and functionality for 100g probe

--- app.py
+++ app.py
@@ -2,42 +2,49 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
+AVG_SIMPLIFIED = 6.02214
+
 root = Tk()
 root.title("at-to-mass-Converter")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-mainframe1 = ttk.Frame(root, padding="3 3 12 12")
+mainframe1 = ttk.Frame(root, padding="3 3 12 12", width=200, height=100)
 mainframe1.grid(column=0, row=0, sticky=(N,W,E,S))
 
-mainframe2 = ttk.Frame(root, padding="3 3 12 12")
+mainframe2 = ttk.Frame(root, padding="3 3 12 12", width=100, height=100)
 mainframe2.grid(column=0, row=0, sticky=(N,W,E,S))
 
+class Element:
+    def __init__(self, symbol, atomicWeight):
+        self.symbol = symbol
+        self.atomicWeight = atomicWeight
+
 dict1 = {
-    0: "Zr",
-    1: "Cu",
-    2: "Al", 
-    3: "Ni", 
-    4: "Ag", 
-    5: "Fe", 
-    6: "Si", 
-    7: "Sn", 
-    8: "B", 
-    9: "C", 
-    10: "P", 
-    11: "Y", 
-    12: "Co", 
-    13: "Mn", 
-    14: "V", 
-    15: "Ca", 
-    16: "Nb", 
-    17: "Cr", 
-    18: "Mo", 
-    19: "Ga", 
-    20: "La", 
-    21: "Nd", 
-    22: "Ti", 
-    23: "W"
+    0: Element("Zr", 91.22),
+    1: Element("Cu", 63.546),
+    2: Element("Al", 26.98154), 
+    3: Element("Ni", 58.70), 
+    4: Element("Ag", 107.868), 
+    5: Element("Fe", 55.847), 
+    6: Element("Si", 28.0855), 
+    7: Element("Sn", 118.69), 
+    8: Element("B", 10.81), 
+    9: Element("C", 12.011), 
+    10: Element("P", 30.97376), 
+    11: Element("Y", 88.9059), 
+    12: Element("Co", 58.9332), 
+    13: Element("Mn", 54.9380), 
+    14: Element("V", 50.9415), 
+    15: Element("Ca", 40.08), 
+    16: Element("Nb", 92.9064), 
+    17: Element("Cr", 51.996), 
+    18: Element("Mo", 95.94), 
+    19: Element("Ga", 69.72), 
+    20: Element("La", 138.9055), 
+    21: Element("Nd", 144.24), 
+    22: Element("Ti", 47.90), 
+    23: Element("W", 183.85)
 }
 
 choices = ["Zr", "Cu", "Al", "Ni", "Ag", "Fe", "Si", "Sn", "B", "C", "P", "Y", "Co", "Mn", "V", "Ca", "Nb", "Cr", "Mo", "Ga", "La", "Nd", "Ti", "W"]
@@ -50,17 +57,28 @@ def whatsSelected():
             messagebox.showinfo(message='Please choose at least two elements')
             return
         
-        
-        mainframe2.tkraise()
+        ttk.Label(mainframe2, text="Enter %at. of each element ").grid(column=1, row=1, columnspan=2 ,sticky=(W,E))
 
         for i, item in enumerate(elList.curselection()):
-            print(dict1[item])
+            print(dict1[item].symbol)
 
-            lblElement = ttk.Label(mainframe2, text=dict1[item] + ": ")
-            lblElement.grid(column=1, row=i+2, sticky=(E))
+            lblElement = ttk.Label(mainframe2, name="lbl"+str(i), text=dict1[item].symbol + ": ")
+            lblElement.grid(column=1, row=i+2, sticky=W)
 
-            tbElement = ttk.Entry(mainframe2, name="tb"+str(i), width=5)
-            tbElement.grid(column=2, row=i+2, sticky=(W))
+            tbElement = ttk.Entry(mainframe2, name="tbPrc"+str(i), width=5)
+            tbElement.grid(column=1, row=i+2, sticky=E)
+
+            tbMass = ttk.Entry(mainframe2, name="tbMass"+str(i))
+            # tbMass.configure(state="readonly")
+            tbMass.grid(column=2, row=i+2, sticky=E)
+
+        btnBack = ttk.Button(mainframe2, text="BACK", command=goBack, name="btnBack")
+        btnBack.grid(column=1, row=len(elList.curselection()) + 2, sticky=W)
+
+        btnCalculate = ttk.Button(mainframe2, text="CALCULATE", command=calculate, name="btnCalculate")
+        btnCalculate.grid(column=2, row=len(elList.curselection()) + 2, sticky=E)
+
+        mainframe2.tkraise()
 
     except ValueError:
         pass
@@ -68,8 +86,26 @@ def whatsSelected():
 def goBack():
     try:
         mainframe1.tkraise()
+
+        for child in mainframe2.winfo_children(): 
+            child.destroy()
+
     except ValueError:
         pass
+
+def calculate():
+    controlSum = 0
+    for i, item in enumerate(elList.curselection()):
+        atInput = float(mainframe2.nametowidget("tbPrc"+str(i)).get())
+        controlSum = controlSum + (atInput * dict1[item].atomicWeight) / AVG_SIMPLIFIED
+
+    for i, item in enumerate(elList.curselection()):
+        atInput = float(mainframe2.nametowidget("tbPrc"+str(i)).get())
+        answer = ((atInput * dict1[item].atomicWeight) / AVG_SIMPLIFIED ) / controlSum * 100
+        mainframe2.nametowidget("tbMass"+str(i)).insert(0, str(answer))
+    
+    print(controlSum)
+
 
 
 # FRAME 1
@@ -79,14 +115,7 @@ elList.grid(column=1, row=1, sticky=(W))
 ttk.Label(mainframe1, text="PRESS `CONTROL` WHEN SELECTING MULTIPLE ELEMENTS").grid(column=1, row=2, columnspan=3 ,sticky=W)
 
 btnGoToPage2 = ttk.Button(mainframe1, text="NEXT", command=whatsSelected)
-btnGoToPage2.grid(column=2, row=1, sticky=(S))
-
-# FRAME 2
-
-lblColumn1 = ttk.Label(mainframe2, text="Enter %at. of each element ").grid(column=1, row=1, columnspan=2 ,sticky=(W,E))
-
-btnGoToPage1 = ttk.Button(mainframe2, text="BACK", command=goBack)
-btnGoToPage1.grid(column=2, row=5, sticky=S)
+btnGoToPage2.grid(column=2, row=1, sticky=(E, S))
 
 
 # def calculate(*args):
@@ -120,9 +149,5 @@ btnGoToPage1.grid(column=2, row=5, sticky=S)
 for child in mainframe1.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
-# feet_entry.focus()
-# root.bind("<Return>", calculate)
-
 mainframe1.tkraise()
-root.resizable(True, True)
 root.mainloop()
