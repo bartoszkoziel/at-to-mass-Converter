499567ed3ac53feaed33215e2e2593c868a0ad07
bartoszkoziel <kozielbartosz1@gmail.com>
2023-07-04 21:49:31 +0200

    added input value checking

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from tkinter import ttk
 from tkinter import messagebox
 
 AVG_SIMPLIFIED = 6.02214
-tempvar = 0
+elList = []
 
 root = Tk()
 root.title("at-to-mass-Converter")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-mainframe1 = ttk.Frame(root, padding="3 3 12 12", width=200, height=100)
+mainframe1 = ttk.Frame(root, padding="3 3 12 12")
 mainframe1.grid(column=0, row=0, sticky=(N,W,E,S))
 
-mainframe2 = ttk.Frame(root, padding="3 3 12 12", width=100, height=100)
+mainframe2 = ttk.Frame(root, padding="3 3 12 12")
 mainframe2.grid(column=0, row=0, sticky=(N,W,E,S))
 
 class Element:
@@ -53,18 +53,16 @@ choicesvar = StringVar(value=choices)
 
 def whatsSelected():
     try:
-        global tempvar
-        tempvar = elList.curselection()
-        tabTamp = elList.curselection()
-        print("TEB_TEMP: ", tabTamp)
+        global elList
+        elList = lbElements.curselection()
 
-        if len(elList.curselection()) < 2:
+        if len(elList) < 2:
             messagebox.showinfo(message='Please choose at least two elements')
             return
         
         ttk.Label(mainframe2, text="Enter %at. of each element ").grid(column=1, row=1, columnspan=2 ,sticky=(W,E))
 
-        for i, item in enumerate(elList.curselection()):
+        for i, item in enumerate(elList):
             print(dict1[item].symbol)
 
             lblElement = ttk.Label(mainframe2, name="lbl"+str(i), text=dict1[item].symbol + ": ")
@@ -78,18 +76,18 @@ def whatsSelected():
 
         # BACK BUTTON
         btnBack = ttk.Button(mainframe2, text="BACK", command=goBack, name="btnBack")
-        btnBack.grid(column=1, row=len(elList.curselection()) + 4, sticky=W)
+        btnBack.grid(column=1, row=len(elList) + 4, sticky=W)
 
         # LABEL + ALLOY WEIGHT
         lblElement = ttk.Label(mainframe2, text="Enter alloy weight: ")
-        lblElement.grid(column=1, row=len(elList.curselection()) + 3, sticky=E)
+        lblElement.grid(column=1, row=len(elList) + 3, sticky=E)
 
         tbElement = ttk.Entry(mainframe2, name="tbMass", width=5)
-        tbElement.grid(column=2, row=len(elList.curselection()) + 3, sticky=W)
+        tbElement.grid(column=2, row=len(elList) + 3, sticky=W)
 
         # BTN CALCULATE
         btnCalculate = ttk.Button(mainframe2, text="CALCULATE", command=calculate, name="btnCalculate")
-        btnCalculate.grid(column=2, row=len(elList.curselection()) + 4, sticky=E)
+        btnCalculate.grid(column=2, row=len(elList) + 4, sticky=E)
 
         mainframe2.tkraise()
 
@@ -98,81 +96,51 @@ def whatsSelected():
 
 def goBack():
     try:
-        mainframe1.tkraise()
 
         for child in mainframe2.winfo_children(): 
             child.destroy()
+        mainframe1.tkraise()
 
     except ValueError:
         pass
 
 def calculate():
-
-    global tempvar
-
-    tabHelper = tempvar
-
-    print("TAB_HELPER: ", tabHelper)
-    print("TEMPVAR: ", tempvar)
+    global elList
     controlSum = 0
-    print("HERE: ", tabHelper)
-    for i, item in enumerate(tabHelper):
-        atInput = float(mainframe2.nametowidget("tbPrc"+str(i)).get())
+
+    sampleMass = mainframe2.nametowidget("tbMass").get()
+
+    if not sampleMass.isnumeric():
+        messagebox.showinfo(message='Please enter the sample mass')
+        return
+    
+    sampleMass = float(sampleMass)
+
+    for i, item in enumerate(elList):
+        atInput = mainframe2.nametowidget("tbPrc"+str(i)).get()
+
+        if not atInput.isnumeric():
+            messagebox.showinfo(message='Please fill out the form')
+            return
+
+        atInput = float(atInput)
         controlSum = controlSum + (atInput * dict1[item].atomicWeight) / AVG_SIMPLIFIED
 
-        # print("====================")
-        # print("CONTROL SUM: ", controlSum)
-        # print("atInput: ", atInput)
-        # print("dict1[item].atomicWeight", dict1[item].atomicWeight)
-        # print("AVG_SIMPLIFIED: ", AVG_SIMPLIFIED)
-
-    for i, item in enumerate(tabHelper):
-        sampleMass = float(mainframe2.nametowidget("tbMass").get())
+    for i, item in enumerate(elList):
+        
         atInput = float(mainframe2.nametowidget("tbPrc"+str(i)).get())
-        answer = (((atInput * dict1[item].atomicWeight) / AVG_SIMPLIFIED ) / controlSum) * sampleMass
+        answer = round(atInput * dict1[item].atomicWeight / AVG_SIMPLIFIED  / controlSum * sampleMass, 4)
         mainframe2.nametowidget("tbMass"+str(i)).insert(0, str(answer))
-    
-    print(controlSum)
-
 
 
 # FRAME 1
-elList = Listbox(mainframe1, listvariable=choicesvar, selectmode=EXTENDED, name="lbElements")
-elList.grid(column=1, row=1, sticky=(W))
+lbElements = Listbox(mainframe1, listvariable=choicesvar, selectmode=EXTENDED, name="lbElements")
+lbElements.grid(column=1, row=1, sticky=(W))
 
 ttk.Label(mainframe1, text="PRESS `CONTROL` WHEN SELECTING MULTIPLE ELEMENTS").grid(column=1, row=2, columnspan=3 ,sticky=W)
 
 btnGoToPage2 = ttk.Button(mainframe1, text="NEXT", command=whatsSelected)
 btnGoToPage2.grid(column=2, row=1, sticky=(E, S))
-
-
-# def calculate(*args):
-#     try:
-#         value = float(feet.get())
-#         meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-#     except ValueError:
-#         pass
-
-# root = Tk()
-# root.title("Feet to Meters")
-
-# mainframe1 = ttk.Frame(root, padding="3 3 12 12")
-# mainframe1.grid(column=0, row=0, sticky=(N, W, E, S))
-# root.columnconfigure(0, weight=1)
-# root.rowconfigure(0, weight=1)
-
-# feet = StringVar()
-# feet_entry = ttk.Entry(mainframe1, width=7, textvariable=feet)
-# feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-# meters = StringVar()
-# ttk.Label(mainframe1, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-# ttk.Button(mainframe1, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-# ttk.Label(mainframe1, text="feet").grid(column=3, row=1, sticky=W)
-# ttk.Label(mainframe1, text="is equivalent to").grid(column=1, row=2, sticky=E)
-# ttk.Label(mainframe1, text="meters").grid(column=3, row=2, sticky=W)
 
 for child in mainframe1.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
